d5d8cea54e4cf533fccc1f8b1345278d60e7ac5f
TshegoB4 <tshegofatsot005@gmail.com>
2025-08-01 23:39:07 +0200

    Create badges_definition.js

---
+++ badges_definition.js
@@ -0,0 +1,22 @@
+// Badge Definitions Collection
+{
+  _id: ObjectId("..."),
+  badge_type: "weather_warrior",
+  name: "Weather Warrior",
+  description: "Demonstrated exceptional weather awareness",
+  levels: {
+    bronze: {
+      criteria: "Avoided 1 severe weather event",
+      icon_url: "/badges/weather-warrior-bronze.png"
+    },
+    silver: {
+      criteria: "Consistently rerouted around bad weather",
+      icon_url: "/badges/weather-warrior-silver.png"
+    },
+    gold: {
+      criteria: "Mastered advanced weather avoidance",
+      icon_url: "/badges/weather-warrior-gold.png"
+    }
+  }
+}
+
