f6639d95311f4e03da0b6e3da0b7d0f460145d55
Nqabenhle <vboxuser@Xubuntu2.myguest.virtualbox.org>
2025-08-01 23:59:37 +0200

    Add car model

--- api/models/users.js
+++ api/models/users.js
@@ -8,6 +8,12 @@ const UserSchema = new Schema({
   username: { type: String, minLength: 2, maxLength: 50, required},
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, minLength: 6, required: true },
+  id: { type: String, minLength: 13, maxLength: 13, required: true },
+  car: {
+    model: { type: String, minLenght: 3, required: true },
+    numberPlate: { type: String, minLength: 10, maxLength: 20, required: true },
+    releaseDate: { type: Date, required: true },
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
