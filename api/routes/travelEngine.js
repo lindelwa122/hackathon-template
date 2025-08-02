2240db9691fa36020219261094ec1e909ba5904b
TshegoB4 <tshegofatsot005@gmail.com>
2025-08-02 08:53:57 +0200

    Comment some stuff

--- api/routes/travelEngine.js
+++ api/routes/travelEngine.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const travelEngineController = require('../controllers/auth');
 
-router.get('/prediction', travelEngineController.overallPoints);
-router.get('/historical-data', travelEngineController.discounts);
+// router.get('/prediction', travelEngineController.overallPoints);
+// router.get('/historical-data', travelEngineController.discounts);
 
 module.exports = router;
