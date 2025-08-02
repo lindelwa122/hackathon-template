c8ea289720dad4143acb5410b409032e93622b00
TshegoB4 <tshegofatsot005@gmail.com>
2025-08-02 08:41:53 +0200

    Add badges controller

--- api/controllers/badges.js
+++ api/controllers/badges.js
@@ -1,7 +1,41 @@
 const Badges = require('./models/badges');
 const BadgesSerializer = require('../serializers/badgesSerializer');
+const profileSerializer = require('../serializers/profileSerializer');
 const asyncHandler = require('express-async-handler');
+const Profile = require('../models/profile');
 
 exports.all = asyncHandler(async (req, res, next) => {
+    const badges = await Badges.find({});
+    const serializedData = new BadgesSerializer(badges);
+    return res.status(200).json({
+        data: serializedData
+    });
+});
+
+exports.acquired = asyncHandler(async (req, res, next) => {
+    const profile = await Profile.find({ user_id: req.user._id });
+    const serializedData = new profileSerializer(profile);
+    serializedData.include('badges');
+    return res.status(200).json({
+        data: serializedData
+    });
+});
+
+exports.current = asyncHandler(async (req, res, next) => {
+    const profile = await Profile.find({ user_id: req.user._id })[0];
+    const riskyScore = profile.risky_score;
+    const badges = profile.badges;
+
+    const acquiredBadges = [];
+    for (const badge of badges) {
+        if (badge.threshold >= riskyScore) {
+            acquiredBadges.push(badge);
+        }
+    }
+
+    const currentBadge = acquiredBadges.sort((a, b) => a.threshold - b.threshold);
     
+    return res.status(200).json({
+        data: currentBadge
+    });
 });
